427282896002dbd75ed7d8b8437cabe85bb31eca
chrfrasco <christianfscott@gmail.com>
2017-09-21 12:56:10 +1200

    Symlink ftplugin dir rather than each file

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-echo "Copying ~/dotfiles/init.vim to ~/.config/nvim/init.vim"
-ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
-echo "...done"
+
+function try_link() {
+  if [ ! -f $2 ]; then
+    ln -s $1 $2 && echo "symlinked $2"
+  else
+    echo "$2 already exists; continuing"
+  fi
+}
+
+# Copy init.vim
+try_link ~/dotfiles/init.vim ~/.config/nvim/init.vim
+
+# Copy all ftplugin files
+try_link ~/dotfiles/ftplugin ~/.config/nvim/ftplugin
+
+# Copy .vimrc
+try_link ~/dotfiles/vimrc ~/.vimrc
